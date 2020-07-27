cce710f1d59ff11a4f7396fbd64dcc6d7031b9f0
Jamile Lima <jamilelima@protonmail.com>
2020-07-27 09:32:29 -0300

    chore: init main activity layout; setup toolbar and colors

--- app/src/main/java/dev/jamile/MainActivity.kt
+++ app/src/main/java/dev/jamile/MainActivity.kt
@@ -2,10 +2,22 @@ package dev.jamile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+
+    private fun setupToolbar() {
+        supportActionBar?.apply {
+            setCustomView(R.layout.custom_toolbar)
+            displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+            elevation = 0F
+            actionBar?.elevation = 0F
+        }
     }
 }
