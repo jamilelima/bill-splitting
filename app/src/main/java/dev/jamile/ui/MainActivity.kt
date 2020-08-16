72e790af6fa954a8cb59bd6a960e7cbb89b71783
Jamile <jamilelima@protonmail.com>
2020-08-16 18:28:27 -0300

    feat: setup main activity

--- app/src/main/java/dev/jamile/ui/MainActivity.kt
+++ app/src/main/java/dev/jamile/ui/MainActivity.kt
@@ -1,22 +1,15 @@
-package dev.jamile
+package dev.jamile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
+import dev.jamile.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupToolbar()
-    }
-
-
-    private fun setupToolbar() {
-        supportActionBar?.apply {
-            setCustomView(R.layout.custom_toolbar)
-            displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-            elevation = 0F
-        }
     }
 }
